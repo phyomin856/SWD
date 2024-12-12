6870c8337a5a83cab6d74e401da0fba718d13ff9
phyomin856 <phyominkhant106@gmail.com>
2024-12-12 21:40:52 +0800

    OOP intro

---
+++ Learn JS/introOop.js
@@ -0,0 +1,87 @@
+// const car = {
+//     model : "2021",
+//     brand : "Kia",
+//     enginePower : 1.5,
+//     drive(){
+//         return this.brand + " can drive."
+//     },
+//     brake(){
+//         return this.brand + " can stop."
+//     }
+// }
+// console.log(car);
+// console.log(car.enginePower);
+// console.log(car.model);
+// console.log(car.brand);
+// console.log(car.drive());
+// console.log(car.brake());
+
+// class Car{
+//     // model = "2021";
+//     // brand = "Kia";
+//     // enginePower = 1.5;
+//     constructor(inputModel,inputBrand){
+//         this.model = inputModel;
+//         this.brand = inputBrand;
+//     };
+//     drive(){
+//         return this.model + " can drive."
+//     };
+//     brake(){
+//         return this.model + " can stop."
+//     }
+   
+
+// }
+// const x = new Car("K5","KIA")
+// console.log(x);
+// console.log(x.drive());
+
+// const y = new Car("Accord","Hondo")
+// console.log(y);
+
+// const z = new Car("Sonata","Hyundai")
+// console.log(z);
+// console.log(x.drive());
+// console.log(Car);
+// neet to chg from class to obj(instantiate), new keyword ko use
+// const x = new Car(222,333)
+// console.log(Car.drive());
+// console.log(x);
+// console.log(x.drive());
+// console.log(x.brake());
+
+// class Car{
+//     static model = 2023
+//     static brand = "KIA K5"
+//     #enginePower = "2000CC"
+//     drive(){
+//         return this.brand + " can drive and it has " + this.#enginePower 
+//     }
+//     static brake(){
+//         return this.brand + " can brake "
+//     }
+// }
+// console.log(Car.model);
+// console.log(Car.brand);
+// console.log(Car.brake());
+// const x = new Car();
+// console.log(x);
+// console.log(x.model);
+// console.log(x.brand);
+// console.log(x.drive());
+// console.log(x.#enginePower);
+
+console.log(Date.now());
+console.log(Math.PI);
+console.log(Math.pow(3,4));
+
+const date = new Date();
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.getDate());
+console.log(date.getTime());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getDate());
+console.log(date.getMonth());
