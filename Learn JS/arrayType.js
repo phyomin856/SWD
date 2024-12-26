5a7e3d04b8af283f4a402744e56a089b2dc7b048
phyomin856 <phyominkhant106@gmail.com>
2024-12-26 17:08:05 +0800

    day35-second video

---
+++ Learn JS/arrayType.js
@@ -0,0 +1,18 @@
+// const fruits = [ "Lemon", "Cherry", "Date", "Banana","Elderberry", "Fig", "Honeydew","Grapes", "Apple", "Kiwi"];
+// console.log(fruits);
+// console.log(fruits.length);
+// console.log(fruits.toString());
+// console.log(fruits.join());
+// console.log(fruits.join(" + "));
+// console.log(fruits.join("\n")); 
+// console.log(fruits.sort());
+// console.log(fruits.slice(0,3));
+// console.log(fruits);
+// console.log(fruits.splice(3,4));
+// console.log(fruits);
+const num = [1,2,3,4,5,6]
+const str = ["www","rrrr","yyyy"]
+const emoji = ["😂","😩"]
+// const result = num.concat(str).concat(emoji)
+// console.log(result);
+// console.log(num.concat(str).concat(emoji)); 
